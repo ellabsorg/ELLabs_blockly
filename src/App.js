bcca946e9370f538cdaf7e838c1161cec55f4769
ishakboufatah <boufatah.ishak@gmail.com>
2024-07-25 01:49:31 +0100

    add ToolBox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 
-import BlocklyComponent, { Block, Value, Field, Shadow } from "react-blockly";
 import { BlocklyWorkspace } from "react-blockly";
 
 const TestBlockly = () => {
@@ -11,41 +10,59 @@ const TestBlockly = () => {
     '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
 
   const toolboxConfiguration = {
-    kind: "categoryToolbox",
-    contents: [
+    "kind": "categoryToolbox",
+    "contents": [
       {
-        kind: "block",
-        type: "controls_if",
+        "kind": "category",
+        "name": "Control",
+        "colour": "#5CA65C",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "controls_if"
+          },
+        ]
       },
       {
-        kind: "block",
-        type: "controls_whileUntil",
+        "kind": "category",
+        "name": "Logic",
+        "colour": "#5C81A6",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "logic_compare"
+          },
+          {
+            "kind": "block",
+            "type": "logic_operation"
+          },
+          {
+            "kind": "block",
+            "type": "logic_boolean"
+          }
+        ]
       },
-      {
-        kind: "block",
-        type: "logic_compare",
-      },
-      {
-        kind: "block",
-        type: "math_number",
-      },
-    ],
+      
+    ]
   };
+  
 
   return (
     <div style={{ height: "500px", backgroundColor: "rgba(21,22,69, 0.2)" }}>
       <BlocklyWorkspace
+        // toolboxCategories={toolboxCategories}
         toolboxConfiguration={toolboxConfiguration}
         initialXml={initialXml}
         className="fill-height"
-        onJsonChange={(json) => {
-          setJson(json);
-          console.log(json);
-        }}
+        // onJsonChange={(json) => {
+        //   setJson(json);
+        //   console.log(json);
+        // }}
       />
       <textarea
         id="code"
         style={{ height: "200px", width: "400px" }}
+        value=""
         readOnly
       ></textarea>
     </div>
